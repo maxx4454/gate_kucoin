3fa7794d33855a958fb581853707e953b615a8f8
maxx4454 <filippov.maks.d@gmail.com>
2021-12-06 02:16:06 +0300

    upd

--- src/main/java/FileLoader.java
+++ src/main/java/FileLoader.java
@@ -4,6 +4,7 @@ import java.net.URL;
 public class FileLoader {
     static void writeFile (String s) throws IOException {
         URL resource = FileLoader.class.getClassLoader().getResource("parser.txt");
+        System.out.println(resource);
         FileWriter myWriter;
         myWriter = new FileWriter(String.valueOf(resource));
         myWriter.write(s);
@@ -12,6 +13,7 @@ public class FileLoader {
 
     static String readFile (String name) throws IOException {
         InputStream is = FileLoader.class.getResourceAsStream("/" + name);
+        System.out.println(FileLoader.class.getClassLoader().getResource("old_coin.txt"));
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         return reader.readLine();
     }
